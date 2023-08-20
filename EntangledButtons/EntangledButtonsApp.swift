b63642bf2fb53ca9438d5f3ad5285275c774f72f
Zach Coriarty <zackcor27@gmail.com>
2023-08-20 10:15:35 -0400

    Initial Commit

---
+++ EntangledButtons/EntangledButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntangledButtonsApp.swift
+//  EntangledButtons
+//
+//  Created by Zachary Coriarty on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct EntangledButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
